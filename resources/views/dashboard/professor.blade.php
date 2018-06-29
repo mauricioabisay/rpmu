1079eff9e733babf39443a504d5e35ed4d9ca1b7
Mauricio Abisay <mauricioabisay.lopez@gmail.com>
2018-06-29 10:52:50 -0700

    Professor dashboard mockup

--- resources/views/dashboard/professor.blade.php
+++ resources/views/dashboard/professor.blade.php
@@ -6,4 +6,77 @@
 		<p>Bienvenido Profesor {{ Auth::user()->name }}</p>
 	</div>
 </div>
+
+<div class="row">
+    <div class="col-sm-12 col-lg-6">
+        <div class="card">
+            <div class="card-body">
+                <h5 class="card-title">Avance de mis Investigaciones</h5>
+                <canvas id="rpm-researches-by-status"></canvas>
+            </div>
+        </div>
+    </div>
+    <div class="col-sm-12 col-lg-6">
+        <div class="card">
+            <div class="card-body">
+                <h5 class="card-title">Objetivos de Investigaciones</h5>
+                <canvas id="rpm-researches-by-faculty"></canvas>
+            </div>
+        </div>
+    </div>  
+</div>
+
+<script type="text/javascript">
+    new Chart(
+        document.getElementById('rpm-researches-by-status').getContext('2d'),
+        {
+            type: 'pie',
+            data: {
+                labels: ['Creadas', 'En desarrollo', 'Terminadas'],
+                datasets: [{
+                    label: 'Dataset',
+                    backgroundColor: ['#ddd', '#dfd', '#fdd'],
+                    data: [2,1,0]
+                }]
+            },
+            options: {
+                legend: {
+                    position: 'right'
+                }
+            }
+        }
+    );
+    new Chart(
+        document.getElementById('rpm-researches-by-faculty').getContext('2d'),
+        {
+            type: 'bar',
+            data: {
+                labels: ['Investigacion 1', 'Investigacion 2', 'Investigacion 3', 'Investigacion 4'],
+                datasets: [
+                    {
+                        label: 'Pendientes',
+                        data: [5,3,2,4],
+                        backgroundColor: '#ddd'
+                    },
+                    {
+                        label: 'Cumplidos',
+                        data: [1,7,1,10],
+                        backgroundColor: '#dfd'
+                    }
+                ]
+            },
+            options: {
+                legend: {
+                    position: 'right'
+                },
+                scales: {
+                    xAxes: [{stacked: true}],
+                    yAxes: [{stacked: true}]
+                },
+                responsive: true,
+            }
+        }
+    );
+</script>
+
 @endsection
