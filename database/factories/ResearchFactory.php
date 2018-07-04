bf242f8dcaa7c91c22c4222984ce7b0c3e41070c
Mauricio Abisay <mauricioabisay.lopez@gmail.com>
2018-07-04 12:53:55 -0500

    Added dummy data via laravel seeder

--- database/factories/ResearchFactory.php
+++ database/factories/ResearchFactory.php
@@ -3,7 +3,15 @@
 use Faker\Generator as Faker;
 
 $factory->define(App\Research::class, function (Faker $faker) {
+	$title = $faker->text();
+	$slug = str_slug($title);
     return [
-        //
+    	'slug' => $slug,
+    	'title' => $title,
+    	'abstract' => $faker->paragraph(1),
+    	'status' => $faker->randomElement(['created', 'started', 'completed']),
+    	'subject' => App\Subject::all()->random()->slug,
+    	'description' => $faker->paragraph(3),
+    	'extra_info' => '',
     ];
 });
--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -17,13 +17,49 @@ class DatabaseSeeder extends Seeder
         factory(App\Faculty::class, 3)->create()->each(function ($f) {
         	factory(App\Degree::class, 3)->create([
         		'faculty_slug' => $f->slug
-        	])->each(function ($d) {
-                $faker = Faker\Factory::create();
-                factory(App\Participant::class)->create([
-                    'id' => $faker->unique()->randomDigit,
-                    'degree_slug' => $d->slug
-                ]);
-            });
+        	]);
+        });
+
+
+        factory(App\Participant::class, 10)->create();
+
+        factory(App\Participant::class)->create([
+            'id' => '24400004',
+            'name' => 'MAURICIO ABISAY LOPEZ VELAZQUEZ',
+            'slug' => 'mauricio_abisay_lopez_velazquez',
+            'user_id' => factory(App\User::class)->create([
+                'email' => 'mauricioabisay.lopez@upaep.edu.mx',
+                'role' => 'admin'
+            ])->id
+        ]);
+
+        factory(App\Participant::class)->create([
+            'id' => '24400005',
+            'name' => 'MAURICIO ABISAY',
+            'slug' => 'mauricio_abisay',
+            'user_id' => factory(App\User::class)->create([
+                'email' => 'mauricioabisay.lopez@gmail.com',
+                'role' => 'admin'
+            ])->id
+        ]);
+
+        factory(App\Research::class, 20)->create()->each(function ($r) {
+            factory(App\Goal::class, 5)->create([
+                'research_id' => $r->id
+            ]);
+            factory(App\Requirement::class, 5)->create([
+                'research_id' => $r->id
+            ]);
+        });
+
+        App\Research::all()->each(function ($r) {
+            $r->participants()->attach(
+                App\Participant::all()->random()->id, 
+                [
+                    'role' => 'leader',
+                    'assigment' => ''
+                ]
+            );
         });
     }
 }
